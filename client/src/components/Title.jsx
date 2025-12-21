64a8ba9560c1ad0aa2ce56e8305c6740ad3e610f
Pranav2325 <pranavdhondkar@gmail.com>
2025-12-21 20:52:30 +0530

    create filter search bar,platform filter and price range option

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Title = ({title,description}) => {
   return (
-    <div className='flex flex-col items-center mb-8'>
+    <div className='flex flex-col items-center mb-8 text-center px-4'>
         <h3 className='text-2xl font-bold text-gray-800'>{title}</h3>
-        <p className='text-slate-600 max-w-[500px]'>{description}</p>
+        <p className='text-slate-600 text-md max-w-md mx-auto'>{description}</p>
 
        
     </div>
--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -23,6 +23,7 @@ const Marketplace = () => {
   
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+      {/* Top bar */}
       {/* back to home and filters button */}
       <div className="flex items-center justify-between text-slate-500">
         <button
